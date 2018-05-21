7dabb9ee5fe12a2100f2f54a88df3af1cafc1fe2
Wouter Devinck <wouter.devinck@gmail.com>
2018-05-21 17:54:21 +0200

    Include lamp version

--- src/mcu/components/updater/component.mk
+++ src/mcu/components/updater/component.mk
@@ -1,8 +1,10 @@
 COMPONENT_ADD_INCLUDEDIRS := .
 COMPONENT_EMBED_FILES := ../../../../build/fpga/lamp.bin
-COMPONENT_EXTRA_CLEAN := fpga_version.h
+COMPONENT_EXTRA_CLEAN := version.h
 
-fpga_version.h:
-	printf "#pragma once\n#define FPGA_HASH \"%s\"\n" `cat ../../../build/fpga/lamp.hash` > $@
+version.h:
+	printf "#pragma once\n#define FPGA_HASH \"%s\"\n#define LAMP_VERSION \"%s\"\n" \
+	  `cat ../../../build/fpga/lamp.hash` \
+	  `git describe --tags --dirty` > $@
 
-Updater.o: fpga_version.h
+Updater.o: version.h

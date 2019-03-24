54c54e1324353adb73281b6a0a4257e6d02a6773
Wouter Devinck <wouter.devinck@gmail.com>
2019-03-24 20:04:56 +0100

    Connect to iot hub - wip

--- src/mcu/components/azureiot/AzureIot.cpp
+++ src/mcu/components/azureiot/AzureIot.cpp
@@ -8,8 +8,46 @@ AzureIot::AzureIot(ILogger* logger) {
   _dps = new DpsClient(logger);
 }
 
+// TODO Make it possible to store registration info to avoid re-doing DPS?
+
 IIotClient* AzureIot::connect(IIotHandler* handler) {
-  auto url = _dps->Register(_idscope);
-  _logger->logInfo(_tag, "IoTHub URL: " + url);
+  auto reg = _dps->Register(_idscope);
+  _logger->logInfo(_tag, "IoTHub URL: " + reg->url);
+
+/*
+  ::IoTHub_Init();
+  _logger->logInfo(_tag, "Iothub API Version: " + string(::IoTHubClient_GetVersionString()));
+
+  auto statusCallback = [] (IOTHUB_CLIENT_CONNECTION_STATUS result, IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason, void* ctx) -> void { 
+    (void)reason;
+    if (result == IOTHUB_CLIENT_CONNECTION_AUTHENTICATED) {
+      auto that = (AzureIot*)ctx;
+
+    }
+  };
+
+  IOTHUB_DEVICE_CLIENT_LL_HANDLE handle;
+  if ((handle = ::IoTHubDeviceClient_LL_CreateFromDeviceAuth(reg->url, reg->deviceid, MQTT_Protocol)) == NULL) {
+    // printf("failed create IoTHub client from connection string %s!\r\n", user_ctx.iothub_uri);
+  } else {
+    ::IoTHubDeviceClient_LL_SetConnectionStatusCallback(handle, statusCallback, &iothub_info);
+    
+    // ::IoTHubDeviceClient_LL_SetMessageCallback(handle, receive_msg_callback, &iothub_info);
+
+    // IOTHUB_MESSAGE_HANDLE msg_handle = ::IoTHubMessage_CreateFromByteArray((const unsigned char*)msgText, strlen(msgText));
+    // ::IoTHubDeviceClient_LL_SendEventAsync(handle, msg_handle, NULL, NULL) != IOTHUB_CLIENT_OK)
+    // ::IoTHubMessage_Destroy(msg_handle);
+                    
+    // ::IoTHubDeviceClient_LL_DoWork(handle);
+    // ::ThreadAPI_Sleep(1);
+          
+    //::IoTHubDeviceClient_LL_Destroy(handle);
+    //::IoTHub_Deinit();
+  }
+*/
+
   return new AzureIotClient();
 }
+
+// IoTHubDeviceClient_LL_SetOption(handle, OPTION_TRUSTED_CERT, certificates);
+// IoTHubDeviceClient_LL_SetOption(handle, OPTION_LOG_TRACE, true);
--- src/mcu/components/azureiot/DpsClient.cpp
+++ src/mcu/components/azureiot/DpsClient.cpp
@@ -15,38 +15,38 @@ DpsClient::DpsClient(ILogger* logger) {
   _logger = logger;
 }
 
-string DpsClient::Register(string idscope) {
+DpsResult* DpsClient::Register(string idscope) {
   DpsContext ctx;
-  ctx.registered = false;
+  ctx.result = new DpsResult();
   ctx.dps = this;
-
+  ctx.registered = false;
+  ctx.completed = false;
   ::prov_dev_security_init(SECURE_DEVICE_TYPE_X509);
   _logger->logInfo(_tag, "Provisioning API Version: " + string(::Prov_Device_LL_GetVersionString()));
-
   auto statusCallback = [] (PROV_DEVICE_REG_STATUS status, void* ctx) -> void { 
     auto context = (DpsContext*)ctx;
     ostringstream log;
     log << "Provisioning Status: " << (int)status;
     context->dps->_logger->logInfo(context->dps->_tag, log.str());
   };
-
   auto registerCallback = [] (PROV_DEVICE_RESULT result, const char* uri, const char* id, void* ctx) -> void {
     auto context = (DpsContext*)ctx;
     if (result == PROV_DEVICE_RESULT_OK) {
-        context->url = string(uri);
-        context->deviceid = string(id);
+        context->result->url = string(uri);
+        context->result->deviceid = string(id);
         context->registered = true;
+        context->completed = true;
         ostringstream log;
-        log << "Registration succesful: id: " << context->deviceid; 
-        log << " - hub: " << context->url;
+        log << "Registration succesful: id: " << context->result->deviceid; 
+        log << " - hub: " << context->result->url;
         context->dps->_logger->logInfo(context->dps->_tag, log.str());
     } else {
+        context->completed = true;
         ostringstream log;
         log << "Registration failed: " << (int)result;
         context->dps->_logger->logError(context->dps->_tag, log.str());
     }
   };
-
   PROV_DEVICE_LL_HANDLE handle;
   if ((handle = ::Prov_Device_LL_Create(_url.c_str(), idscope.c_str(), ::Prov_Device_MQTT_Protocol)) == NULL) {
     _logger->logError(_tag, "Failed calling Prov_Device_LL_Create");
@@ -57,15 +57,15 @@ string DpsClient::Register(string idscope) {
       do {
         ::Prov_Device_LL_DoWork(handle);
         ::ThreadAPI_Sleep(20);
-      } while (ctx.registered == false); // TODO - endless loop if fails...
+      } while (ctx.completed == false);
     }
     ::Prov_Device_LL_Destroy(handle);
   }
-  // if (user_ctx.registration_complete != 1) {
-  //  _logger->logError(_tag, "Registration failed!");
-  // }
+  if (ctx.registered == false) {
+    _logger->logError(_tag, "Registration failed!");
+  }
   ::prov_dev_security_deinit();
-  return ctx.url; // TODO Error handling?
+  return ctx.result; // TODO Error handling?
 }
 
 // TODO ?

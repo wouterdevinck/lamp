583a0712860068cea48f48c61cbd5b6220694f1f
Wouter Devinck <wouter.devinck@gmail.com>
2019-12-29 16:15:53 +0100

    Refactor IotClient

--- src/mcu/components/azureiot/AzureIotClient.cpp
+++ src/mcu/components/azureiot/AzureIotClient.cpp
@@ -3,7 +3,6 @@
 #include "iothub.h"
 #include "iothubtransportmqtt.h"
 #include "iothub_client_version.h"
-#include "iothub_message.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -11,47 +10,45 @@
 using namespace lamp;
 
 AzureIotClient::AzureIotClient(string hubUrl, string deviceId, IIotHandler* handler, ILogger* logger) {
-  _handler = handler;
-  _logger = logger;
+  _ctx = new IotContext();
+  _ctx->logger = logger;
+  _ctx->tag = _tag;
+  _ctx->handler = handler;
   ::IoTHub_Init();
-  _logger->logInfo(_tag, "Iothub API Version: " + string(::IoTHubClient_GetVersionString()));
-  // TODO use capturing lambda trick? or context struct
-  auto statusCallback = [] (IOTHUB_CLIENT_CONNECTION_STATUS result, IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason, void* ctx) -> void { 
-    (void)reason;
-    if (result == IOTHUB_CLIENT_CONNECTION_AUTHENTICATED) {
-      auto that = (AzureIotClient*)ctx;
-      auto logger = that->getLogger();
-      logger->logInfo("statusCallback", "IOTHUB_CLIENT_CONNECTION_AUTHENTICATED");
-    }
-  };
-  auto messageCallback = [] (IOTHUB_MESSAGE_HANDLE message, void* ctx) -> IOTHUBMESSAGE_DISPOSITION_RESULT { 
-    (void)message;
-    auto that = (AzureIotClient*)ctx;
-    auto logger = that->getLogger();
-    logger->logInfo("messageCallback", "Message received");
-    return IOTHUBMESSAGE_ACCEPTED;
-  };
-  if ((_handle = ::IoTHubDeviceClient_LL_CreateFromDeviceAuth(hubUrl.c_str(), deviceId.c_str(), ::MQTT_Protocol)) == NULL) {
+  logger->logInfo(_tag, "Iothub API Version: " + string(::IoTHubClient_GetVersionString()));
+  _ctx->handle = ::IoTHubDeviceClient_LL_CreateFromDeviceAuth(hubUrl.c_str(), deviceId.c_str(), ::MQTT_Protocol);
+  if (_ctx->handle == NULL) {
     logger->logError(_tag, "Failed to create IoT Hub client from connection string");
   } else {
-    ::IoTHubDeviceClient_LL_SetConnectionStatusCallback(_handle, statusCallback, this);//&iothub_info);
-    ::IoTHubDeviceClient_LL_SetMessageCallback(_handle, messageCallback, this);//&iothub_info);
-    if (::xTaskCreate(&doWork, "doIotWork", 5 * 1024, (void*)_handle, 5, NULL) != pdPASS) {
-      _logger->logError(_tag, "Create task failed");
+    ::IoTHubDeviceClient_LL_SetConnectionStatusCallback(_ctx->handle, &statusCallback, (void*)_ctx);
+    ::IoTHubDeviceClient_LL_SetMessageCallback(_ctx->handle, &messageCallback, (void*)_ctx);
+    if (::xTaskCreate(&doWork, "doIotWork", 5 * 1024, (void*)_ctx, 5, NULL) != pdPASS) {
+      logger->logError(_tag, "Create task failed");
     }
   }
 }
 
-void AzureIotClient::doWork(void* parameter) {
-  auto handle = (IOTHUB_DEVICE_CLIENT_LL_HANDLE)parameter;
+void AzureIotClient::doWork(void* ctx) {
+  auto handle = ((IotContext*)ctx)->handle;
   while(true) {
     ::IoTHubDeviceClient_LL_DoWork(handle);
     ::vTaskDelay(1 / portTICK_PERIOD_MS);
   }
 }
 
-ILogger* AzureIotClient::getLogger() {
-  return _logger;
+void AzureIotClient::statusCallback(IOTHUB_CLIENT_CONNECTION_STATUS result, IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason, void* ctx) {
+  (void)reason;
+  auto that = (IotContext*)ctx;
+  if (result == IOTHUB_CLIENT_CONNECTION_AUTHENTICATED) {
+    that->logger->logInfo(that->tag, "IOTHUB_CLIENT_CONNECTION_AUTHENTICATED");
+  }
+}
+
+IOTHUBMESSAGE_DISPOSITION_RESULT AzureIotClient::messageCallback(IOTHUB_MESSAGE_HANDLE message, void* ctx) {
+  (void)message;
+  auto that = (IotContext*)ctx;
+  that->logger->logInfo(that->tag, "Message received");
+  return IOTHUBMESSAGE_ACCEPTED;
 }
 
 // IOTHUB_MESSAGE_HANDLE msg_handle = ::IoTHubMessage_CreateFromByteArray((const unsigned char*)msgText, strlen(msgText));
--- src/mcu/components/azureiot/AzureIotClient.h
+++ src/mcu/components/azureiot/AzureIotClient.h
@@ -3,6 +3,7 @@
 #include <string>
 
 #include "iothub_device_client_ll.h"
+#include "iothub_message.h"
 
 #include "IIotClient.h"
 #include "IIotHandler.h"
@@ -12,20 +13,24 @@ using namespace std;
 
 namespace lamp {
 
+  struct IotContext {
+    ILogger* logger;
+    string tag;
+    IIotHandler* handler;
+    IOTHUB_DEVICE_CLIENT_LL_HANDLE handle;
+  };
+
   class AzureIotClient : public IIotClient {
 
     public:
       explicit AzureIotClient(string hubUrl, string deviceId, IIotHandler* handler, ILogger* logger);
 
-      ILogger* getLogger();
-
     private:
-      IIotHandler* _handler;
-      ILogger* _logger;
+      IotContext* _ctx;
 
-      IOTHUB_DEVICE_CLIENT_LL_HANDLE _handle;
-
-      static void doWork(void* parameter);
+      static void doWork(void* ctx);
+      static void statusCallback(IOTHUB_CLIENT_CONNECTION_STATUS result, IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason, void* ctx);
+      static IOTHUBMESSAGE_DISPOSITION_RESULT messageCallback(IOTHUB_MESSAGE_HANDLE message, void* ctx);
       
       const string _tag = "AzureIot.AzureIotClient";
 

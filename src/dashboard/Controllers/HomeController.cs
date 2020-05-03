1ab1cda7d98e66cd6364ba9fab642280b8946b6b
Wouter Devinck <wouter.devinck@gmail.com>
2020-05-03 11:36:01 +0200

    Simple dashboard

---
+++ src/dashboard/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using LampControl.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LampControl.Controllers {
+
+    public class HomeController : Controller {
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly DeviceService _device;
+
+        public HomeController(ILogger<HomeController> logger, DeviceService device) {
+            _logger = logger;
+            _device = device;
+        }
+
+        public IActionResult Index() {
+            ViewBag.User = Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"];
+            ViewBag.Devices = _device.GetDevicesAsync();
+            return View();
+        }
+
+        public async Task<IActionResult> Color(string id, string color) {
+            await _device.SetStatusLedColorAsync(id, color);
+            return Redirect("/");
+        }
+
+    }
+
+}

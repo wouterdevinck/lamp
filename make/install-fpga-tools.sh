471a7499ba0a8b03b046ed100ba2ee61f2b2d1e4
Wouter Devinck <wouter.devinck@gmail.com>
2020-08-11 12:11:10 +0200

    Update FPGA tools to latest version and switch from arachne-pnr to nextpnr

--- make/install-fpga-tools.sh
+++ make/install-fpga-tools.sh
@@ -4,31 +4,31 @@ set -ex
 mkdir -p ~/fpga
 
 cd ~/fpga
-git clone https://github.com/cliffordwolf/icestorm.git icestorm
+git clone https://github.com/YosysHQ/icestorm.git icestorm
 cd icestorm
-git checkout 237280ce44f72c7b2e1ca671d5113dba34cc4fca
+git checkout d12308775684cf43ab923227235b4ad43060015e
 make -j$(nproc)
-make install # because chipdb needed by arachne
-make DESTDIR=~/fpga/out install
+make DESTDIR=~/fpga/out PREFIX= install
 
 cd ~/fpga
-git clone https://github.com/cseed/arachne-pnr.git arachne-pnr
-cd arachne-pnr
-git checkout ea2d04215bc0fd6072cda244caeb6670892033b3
+git clone https://github.com/YosysHQ/nextpnr.git nextpnr
+cd nextpnr
+git checkout fbe486df459909065d6852a7495a212dfd2accef
+cmake -DARCH=ice40 -DBUILD_PYTHON=OFF -DBUILD_GUI=OFF -DCMAKE_INSTALL_PREFIX=`echo ~/fpga/out` .
 make -j$(nproc)
-make DESTDIR=~/fpga/out install
+make install
 
 cd ~/fpga
 git clone https://github.com/YosysHQ/yosys.git yosys
 cd yosys
-git checkout 24e64016173f586233e77f4e0d70c7883f98d8f7
+git checkout 04f6158bf2ce9a95970f1be435351786f59bed92
 make -j$(nproc)
 make DESTDIR=~/fpga/out install
 
 cd ~/fpga
-git clone http://git.veripool.org/git/verilator verilator
+git clone https://github.com/verilator/verilator.git verilator
 cd verilator
-git checkout verilator_3_922
+git checkout v4.038
 autoconf
 ./configure --prefix=`echo ~/fpga/out`
 make
@@ -37,7 +37,7 @@ make install
 cd ~/fpga
 git clone https://github.com/steveicarus/iverilog.git iverilog
 cd iverilog
-git checkout v10_2
+git checkout v10_3
 autoconf
 ./configure --prefix=`echo ~/fpga/out`
 make
